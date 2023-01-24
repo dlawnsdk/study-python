981415c0766dc0a82759f2bf71ccfcd2a4b674bb
Juna Im <dlawnsdk04@naver.com>
2023-01-24 18:34:41 +0900

    다양한 그래프 그리기 누적 히스토그램까지

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/NGULIM.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
 import datetime as dt
 """
 시드 설정
@@ -97,30 +102,85 @@ print(x)
 
 """"연습문제 - matplotlib"""
 #np.random.seed(100)
-random_number_1 = np.random.rand(10000) # 균등 난수
-random_number_2 = np.random.randn(10000) # 정규분포
-random_number_3 = np.random.binomial(100, 0.8, size=(100000)) # 이항분포
-
-plt.figure(figsize=(10,10))
-plt.hist(random_number_1, bins=50)
-plt.title('uniform_distribution')
-plt.grid(True)
+# random_number_1 = np.random.rand(10000) # 균등 난수
+# random_number_2 = np.random.randn(10000) # 정규분포
+# random_number_3 = np.random.binomial(100, 0.8, size=(100000)) # 이항분포
+#
+# plt.figure(figsize=(10,10))
+# plt.hist(random_number_1, bins=50)
+# plt.title('uniform_distribution')
+# plt.grid(True)
+# #plt.show()
+#
+# plt.figure(figsize=(10,10))
+# plt.hist(random_number_2, bins=50)
+# plt.grid(True)
 #plt.show()
 
-plt.figure(figsize=(10,10))
-plt.hist(random_number_2, bins=50)
-plt.grid(True)
-#plt.show()
+# plt.figure(figsize=(10,10))
+# plt.hist(random_number_3, bins=50)
+# plt.grid(True)
+# plt.show()
 
-plt.figure(figsize=(10,10))
-plt.hist(random_number_3, bins=50)
-plt.grid(True)
+"""다양한 그래프 그리기"""
+# days = np.arange(1, 11)
+# weight = np.array([10, 14, 12, 18, 20, 16, 18, 17, 11, 21])
+# plt.grid(True)
+# plt.ylim([0, weight.max() + 1])
+# plt.xlabel("days")
+# plt.ylabel("weight")
+#
+# plt.plot(days, weight, marker="s", markerfacecolor="b")
+# plt.show()
+
+"""막대그래프"""
+# x = [1, 2, 3, 4, 5, 6]
+# y = [12, 23, 54, 44, 12, 98]
+# 
+# plt.bar(x, y)
+# plt.show()
+
+"""가로축에 라벨 설정"""
+# x = [1, 2, 3, 4, 5, 6]
+# y = [5, 2, 2, 1, 1, 1]
+# labels = ["사원", "주임", "대리", "선임", "수석", "대표 이사"]
+# plt.title("카이런소프트 임직원 현황")
+# plt.bar(x, y, tick_label=labels, color="darkorange")
+# plt.show()
+
+"""누적 막대 그리기"""
+# x = [1, 2, 3, 4, 5, 6]
+# y1 = [12, 3, 54, 23, 81, 65]
+# y2 = [32, 23, 54, 22, 12, 3]
+# labels = ["orange", "apple", "kiwifruit", "banana", "pineapple", "strawberry"]
+#
+# plt.bar(x, y1, tick_label=labels)
+# plt.bar(x, y2, bottom=y1)
+#
+# plt.legend(("y1", "y2"))
+# plt.show()
+
+"""히스토그램"""
+# np.random.seed(0)
+# data = np.random.randn(10000)
+#
+# plt.hist(data)
+# plt.show()
+
+"""구간수 설정하기"""
+# np.random.seed(0)
+# data = np.random.randn(10000)
+# plt.hist(data, bins=100)
+# plt.show()
+
+"""정규화 하기"""
+# np.random.seed(0)
+# data = np.random.randn(10000)
+# plt.hist(data, bins=100)
+# plt.show()
+
+"""누적 히스토그램 만들기"""
+np.random.seed(0)
+data = np.random.randn(10000)
+plt.hist(data, bins=100, cumulative=True)
 plt.show()
-
-
-
-
-
-
-
-

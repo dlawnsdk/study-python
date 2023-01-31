f80f25fa2e216ca5b31a1f0b7c1a0c609272bf40
Juna Im <dlawnsdk04@naver.com>
2023-01-31 12:59:53 +0900

    람다

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,8 +1,29 @@
-def pow(x):
-    return x ** 2
+"""람다 기초"""
+# a = 4
+# def test(a):
+#     return 2 * a ** 2 - 3 * a + 1
+#
+#
+# test2 = lambda a: 2 * a ** 2 - 3 * a + 1
+#
+# print(test(3))
+# print(test2(3))
 
-pow2 = lambda x: x ** 2
+"""람다에 의한 계산"""
+# x = 5
+# y = 6
+# z = 2
+# def test(x, y, z):
+#     return x*y + z
+#
+# test2 = lambda x, y, z: x*y + z
+# print(test(1, 3, 5))
+# print(test2(1, 3, 5))
 
-a = 4
-b = pow2(a)
-print(b)
+"""IF를 이용한 람다"""
+a1 = 13
+a2 = 32
+
+test = lambda x: x**2 - 40*x + 350 if 10 <= x < 30 else 50
+
+print(test(15))
